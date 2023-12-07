6e3685b1812419e33fb06091d4892234f73a2ec6
diyooor <diyorsattarov7@gmail.com>
2023-12-07 12:41:20 -0600

    Added: - notes regarding the game logic

--- src/game/game.cpp
+++ src/game/game.cpp
@@ -1,3 +1,35 @@
 #include <game/game.h>
 
 Game::Game(int numDecks) : Blackjack(numDecks) {}
+
+void Game::startGame() {
+    /*
+     * Start of a new game:
+     * 1) Dealer is dealt 1 card face up.
+     * 2) Players are dealt 1st card face up.
+     * 3) Players are dealt 2nd card face up.
+     * 4) Dealer is dealt 2nd card face down.
+     *
+     * Triggers for starting a new game:
+     * - Previous game ends.
+     * - Other specific conditions.
+     */
+}
+
+void Game::startRound() {
+    /*
+     * Start of a new round:
+     * - Initialization for player decisions.
+     * - Starting from position 1, players make decisions: hit / stand / split / double / fold.
+     */
+}
+
+void Game::endGame() {
+    /*
+     * End of the game:
+     * - No players remain on the table.
+     * - The shoe has reached the end.
+     *
+     * Additional details about handling winnings/losses or other relevant actions.
+     */
+}
