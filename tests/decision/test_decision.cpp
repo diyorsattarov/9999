d80b9e6b3a710af35cb4a0a779b910e5bc65639b
diyooor <diyorsattarov7@gmail.com>
2023-12-09 10:42:00 -0600

    Added: - logic to iterate through all the player/dealer combinations

--- tests/decision/test_decision.cpp
+++ tests/decision/test_decision.cpp
@@ -10,13 +10,34 @@ protected:
 };
 
 TEST_F(DecisionTest, DecisionsTest) {
-    // Create a new player
     Player player(1);
+    Player player2(2);
+    Dealer dealer;
+    dealer.dealerId = 1;
 
-    // Add a 7 and a 10 to the player's hand
-    player.addCard(Card(CardValue::Seven, CardSuit::Hearts));
-    player.addCard(Card(CardValue::Ten, CardSuit::Spades));
+    for (int dealerCard = 0; dealerCard <= 12; ++dealerCard) {
+        if (dealerCard >= 9 && dealerCard < 12) {
+            continue;
+        }
+        dealer.clearHand();
+        dealer.addCard(Card(static_cast<CardValue>(dealerCard), CardSuit::Spades));
+        dealer.printHand();
+        for (int playerCard = 0; playerCard <= 7; ++playerCard) {
+            player.clearHand();
+            player.addCard(Card(CardValue::Ace, CardSuit::Hearts));
+            player.addCard(Card(static_cast<CardValue>(playerCard), CardSuit::Spades));
+            player.printHand();
+        }
+        // pairs
+        for (int playerCard2 = 0; playerCard2 <= 12; ++playerCard2) {
+            if (playerCard2 >= 9 && playerCard2 < 12) {
+                continue;
+            }
+            player2.clearHand();
+            player2.addCard(Card(static_cast<CardValue>(playerCard2), CardSuit::Spades));
+            player2.addCard(Card(static_cast<CardValue>(playerCard2), CardSuit::Spades));
+            player2.printHand();
 
-    // Test the decision for the player's hand against a dealer card of 10
-    ASSERT_EQ(decision.getDecision(player, 10), PlayerDecision::Stand);
+        }
+    }
 }
